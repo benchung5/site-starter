a9ce15caad7caaf409d36c36973044e7a9cf76df
Ben Chung <ben@benchung.com>
2018-07-19 22:44:49 -0400

    work on auth

--- models/users_model.php
+++ models/users_model.php
@@ -4,13 +4,19 @@ use Lib\Model;
 
 class Users_model extends Model
 {
+	public $options;
+
 	public function __construct() {
 		parent::__construct();
+
+		$this->options = [
+		    'cost' => 11,
+		    'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+		];
 	}
 
 	public function get_user($opts = []) 
 	{
-
 		$this->db->table('users')->select('*');
 
 		if (isset($opts['id'])) {
@@ -21,18 +27,22 @@ class Users_model extends Model
 			$this->db->where('email', '=', $opts['email']);
 		}
 
-		if (isset($opts['password'])) {
-			$this->db->where('password', '=', $opts['password']);
-		}
-
 		$result = $this->db->get();
 
+		if ($result && isset($opts['password'])) {
+			if (! password_verify($opts['password'], $result->password)) {
+				$result = false;
+			}
+		}
+		
 		return $result;
 	}
 
 	public function add_user($data)
 	{
 		if (is_array($data) && isset($data['email'])) {
+
+			$data['password'] = password_hash($data['password'], PASSWORD_BCRYPT, $this->options);
 
 			$this->db->table('users')->insert($data);
 
