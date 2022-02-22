2adf42963241c749df3823917dc0d837e355cef4
Ben Chung <info@naturewithus.com>
2022-02-22 11:50:13 -0500

    updated file names for autoload

---
+++ controllers/Plants.php
@@ -0,0 +1,34 @@
+<?php
+namespace Controllers;
+use Lib\Controller;
+use Lib\Utils;
+
+class Plants extends Controller 
+{
+	public function __construct() 
+	{
+		$this->trees = $this->load_model('trees_model');
+
+		parent::__construct();
+	}
+
+	public function index() 
+	{
+		$this->render('plants', null, 'Niagara Tree Search', 'Search trees, shrubs and woody plants commonly found in the Niagara region');
+
+		$this->load_script('mainPlants.js');
+	}
+
+	public function view($category, $title)
+	{
+		$plant = $this->trees->get(['category' => $category, 'slug' => $title]);
+		
+		if ($plant) {
+			$view_data = [];
+			$view_data['tree'] = $plant;
+			$this->render('view_plant', $view_data, $plant->common_name);
+		} else {
+			$this->render('404');
+		}
+	}
+}
