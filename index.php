682fb4f16f32499da02ed2b713e73a47186b3de4
Ben Chung <ben@benchung.com>
2024-03-25 15:16:54 -0700

    updated

--- index.php
+++ index.php
@@ -12,15 +12,18 @@ $segments = Uri::get_parts();
 
 $controller_dir = isset($segments['controller_dir']) ? $segments['controller_dir'].'/' : '';
 
-$controller = Config::paths('CONTROLLER_PATH').$controller_dir.ucfirst($segments['controller']).'.php';
+//convert first letter to uppercase
+$controller = ucfirst($segments['controller']);
 
-if (file_exists($controller)) {
+$controller_path = Config::paths('CONTROLLER_PATH').$controller_dir.$controller.'.php';
+
+if (file_exists($controller_path)) {
 	$controller_dir_namespace = isset($segments['controller_dir']) ? $segments['controller_dir']."\\" : '';
-	$controller_class_name = "Controllers\\".$controller_dir_namespace.$segments['controller'];
+	$controller_class_name = "Controllers\\".$controller_dir_namespace.$controller;
 	$contr = new $controller_class_name;
 
 	// if it's an article view, handle differently because of category slug
-	if (($segments['controller'] == 'articles' || $segments['controller'] == 'plants') && $segments['action'] && isset($segments['params'][0]) && (!isset($segments['controller_dir']))) {
+	if (($controller == 'Articles' || $controller == 'Plants') && $segments['action'] && isset($segments['params'][0]) && (!isset($segments['controller_dir']))) {
 		call_user_func_array([$contr, 'view'], [$segments['action'], $segments['params'][0]]);
 	} else 	if (method_exists($contr, $segments['action'])) {
 		call_user_func_array([$contr, $segments['action']], $segments['params']);
