afb623b44b62b13021e2d018d8b6ae5ebf72a202
Ben Chung <ben@benchung.com>
2026-03-05 21:18:52 -0800

    updated

--- add_medicinal_species_column.sql
+++
@@ -1,4 +0,0 @@
--- Add medicinal_species column to trees table
--- Run this in phpMyAdmin or MySQL before using the new field.
-
-ALTER TABLE trees ADD COLUMN medicinal_species TEXT NULL AFTER chinese_medicine;
